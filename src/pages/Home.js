55b8479668b9c59c778523524734a0e02b92f810
Jan Lancelot Mailig <maypagasaml@gmail.com>
2024-05-24 21:07:05 +0800

    Modified bg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -92,8 +92,7 @@ const Home = () => {
   return (
     <Layout>
       {/* Hero Section */}
-      <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-        <div className="max-w-7xl mx-auto text-center">
+      <div className="h-[60rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.04] relative overflow-hidden">        <div className="max-w-7xl mx-auto text-center">
           <div className="flex flex-col md:flex-row items-center justify-center">
             <Spotlight
               className="-top-40 left-0 md:left-60 md:-top-20"
